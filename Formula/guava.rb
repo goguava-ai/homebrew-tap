de5f57587d21e8407b3be677d0ad5d2442b3890c
Robbie Jones <robbie@gridspace.com>
2026-08-04 17:06:00 -0700

    Update guava formula to version 0.37.0

--- Formula/guava.rb
+++ Formula/guava.rb
@@ -1,16 +1,16 @@
 class Guava < Formula
   desc "Command-line interface for managing Guava agents and deployments"
   homepage "https://goguava.ai"
-  version "0.36.0"
+  version "0.37.0"
 
   on_macos do
     on_intel do
-      url "https://storage.googleapis.com/gridspace-guava-cli/cli/0.36.0/guava-darwin-x86_64"
-      sha256 "c5042d35e6a8624b2f6f2213b498a9bbae0b91b6795de7509b3e8dde406e7a5b"
+      url "https://storage.googleapis.com/gridspace-guava-cli/cli/0.37.0/guava-darwin-x86_64"
+      sha256 "ae378812e85c38268bc796059cfeee6bf74083d0d27904f8f5dc39a1193578bd"
     end
     on_arm do
-      url "https://storage.googleapis.com/gridspace-guava-cli/cli/0.36.0/guava-darwin-aarch64"
-      sha256 "8ea5c5d67cce1443a925413c3590e0bdef160f6e5a622c7f0f9b371f723ff2fb"
+      url "https://storage.googleapis.com/gridspace-guava-cli/cli/0.37.0/guava-darwin-aarch64"
+      sha256 "e258dc7b0d219c2c5ca8ae49e4b9d72ec50a40fe34ca75a14e330aa567eb17d7"
     end
   end
 
@@ -23,6 +23,13 @@ class Guava < Formula
         "self_update": false
       }
     JSON
+  end
+
+  def caveats
+    <<~EOS
+      #{Tty.green}Next steps:#{Tty.reset}
+        • Run #{Tty.green}guava login#{Tty.reset} to get started.
+    EOS
   end
 
   test do
