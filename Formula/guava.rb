7fedb2ce4e085b95538614ec72a980e4110aaa7b
Robbie Jones <robbie@gridspace.com>
2026-09-01 17:15:28 -0700

    Update guava formula to version 0.41.0

--- Formula/guava.rb
+++ Formula/guava.rb
@@ -1,16 +1,16 @@
 class Guava < Formula
   desc "Command-line interface for managing Guava agents and deployments"
   homepage "https://goguava.ai"
-  version "0.40.0"
+  version "0.41.0"
 
   on_macos do
     on_intel do
-      url "https://storage.googleapis.com/gridspace-guava-cli/cli/0.40.0/guava-darwin-x86_64"
-      sha256 "cc86e129675fdebeb8e9f79c84d23ac46761fb69d90a1214a2ee9b64fba3af00"
+      url "https://storage.googleapis.com/gridspace-guava-cli/cli/0.41.0/guava-darwin-x86_64"
+      sha256 "129d7db37711ddcc8a4db34cbfdb68b10510a8b3d14529f592fcf777a7e79824"
     end
     on_arm do
-      url "https://storage.googleapis.com/gridspace-guava-cli/cli/0.40.0/guava-darwin-aarch64"
-      sha256 "a0a626b9c1d97296ace1fd5b334ac67acad9de29bdc95166f71e4d39a15a20ae"
+      url "https://storage.googleapis.com/gridspace-guava-cli/cli/0.41.0/guava-darwin-aarch64"
+      sha256 "b11b0be48ccc0b2f511fa1e2fa80decbccf324c674a7a5355c3f33b90e8dad84"
     end
   end
 
